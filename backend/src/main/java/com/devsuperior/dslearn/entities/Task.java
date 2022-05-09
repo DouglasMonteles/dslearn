fbc80377d1bf3b2d52ec94b8564b5465685a6525
Douglas Monteles <silvadouglas328@gmail.com>
2022-05-08 23:12:29 -0300

    Lesson, Content and Task

---
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Task.java
@@ -0,0 +1,82 @@
+package com.devsuperior.dslearn.entities;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_task")
+public class Task extends Lesson {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(length = 255, nullable = false)
+	private String description;
+	
+	@Column(nullable = false)
+	private Integer questionCount;
+	
+	@Column(nullable = false)
+	private Integer approvalCount;
+	
+	@Column(nullable = false)
+	private Double weight = 1.0d;
+	
+	@Column(nullable = false, columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant dueDate;
+	
+	public Task() {}
+
+	public Task(Long id, String title, Integer position, Section section, String description, Integer questionCount,
+			Integer approvalCount, Double weight, Instant dueDate) {
+		super(id, title, position, section);
+		this.description = description;
+		this.questionCount = questionCount;
+		this.approvalCount = approvalCount;
+		this.weight = weight;
+		this.dueDate = dueDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getQuestionCount() {
+		return questionCount;
+	}
+
+	public void setQuestionCount(Integer questionCount) {
+		this.questionCount = questionCount;
+	}
+
+	public Integer getApprovalCount() {
+		return approvalCount;
+	}
+
+	public void setApprovalCount(Integer approvalCount) {
+		this.approvalCount = approvalCount;
+	}
+
+	public Double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+
+	public Instant getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Instant dueDate) {
+		this.dueDate = dueDate;
+	}
+	
+}
