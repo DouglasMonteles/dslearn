58eb55f4947117a1e041efff93e5649588449b51
Douglas Monteles <silvadouglas328@gmail.com>
2022-05-09 23:29:23 -0300

    Fix: missing associations, getters

--- backend/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -117,6 +118,23 @@ public class Enrollment implements Serializable {
 
 	public void addDeliver(Deliver deliver) {
 		this.deliveries.add(deliver);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Enrollment other = (Enrollment) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 }
